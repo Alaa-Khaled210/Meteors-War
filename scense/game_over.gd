416b97da65ba83d3151b43caccb369dc806880c4
Alaa-Khaled210 <alaa.khaled.ramadan@gmail.com>
2025-09-21 17:54:47 +0200

    laser destripute meteors

--- scense/game_over.gd
+++ scense/game_over.gd
@@ -6,6 +6,10 @@ extends Control
 func _ready() -> void:
 	$CenterContainer/VBoxContainer/Label2.text=$CenterContainer/VBoxContainer/Label2.text +str(Global.score)
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Shoot"):
+#func _process(_delta: float) -> void:
+	#if Input.is_action_just_pressed("Shoot"):
+		#get_tree().change_scene_to_packed(level_scene)
+		
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Shoot"):
 		get_tree().change_scene_to_packed(level_scene)
