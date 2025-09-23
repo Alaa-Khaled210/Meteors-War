d6407944d5a68fe333f2c4ec9ee9e5dd556b0d4f
Alaa-Khaled210 <alaa.khaled.ramadan@gmail.com>
2025-09-23 10:48:15 +0200

    press space key and check if it visible to move level

--- scense/game_over.gd
+++ scense/game_over.gd
@@ -5,11 +5,15 @@ extends Control
 # Called every frame. 'delta' is the elapsed time since the previous frame.\
 func _ready() -> void:
 	$CenterContainer/VBoxContainer/Label2.text=$CenterContainer/VBoxContainer/Label2.text +str(Global.score)
-
+	$MarginContainer/Label.hide()
+	await get_tree().create_timer(2).timeout
+	$MarginContainer/Label.show()
 #func _process(_delta: float) -> void:
 	#if Input.is_action_just_pressed("Shoot"):
 		#get_tree().change_scene_to_packed(level_scene)
 		
+		
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("Shoot"):
+	if event.is_action_pressed("Shoot") and $MarginContainer/Label.is_visible_in_tree():
+		#await get_tree().create_timer(2).timeout
 		get_tree().change_scene_to_packed(level_scene)
