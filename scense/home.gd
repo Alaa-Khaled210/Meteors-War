cc1b1b0d8a6542f0712893db8711420d2619011f
Alaa-Khaled210 <alaa.khaled.ramadan@gmail.com>
2025-09-25 16:24:29 +0200

    home scene after code it

---
+++ scense/home.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_playgame_pressed() -> void:
+	$click.play()
+	get_tree().change_scene_to_file("res://scense/level.tscn")
+	
+
+
+func _on_instructions_pressed() -> void:
+	$click.play()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_playgame_mouse_entered() -> void:
+	$hover.play()
+
+
+func _on_instructions_mouse_entered() -> void:
+	$hover.play()
+
+
+func _on_exit_mouse_entered() -> void:
+	$hover.play()
