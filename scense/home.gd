22c8e36c04f909d55e03902b4c29d75d5d5d6e0f
Alaa-Khaled210 <alaa.khaled.ramadan@gmail.com>
2025-09-27 00:07:32 +0200

    make star animate in home,instructon

--- scense/home.gd
+++ scense/home.gd
@@ -3,26 +3,39 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	#Stars 
+	var homescreen := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $stars.get_children():
+		#position 
+		var random_x=rng.randi_range(0, int(homescreen.x))
+		var random_y=rng.randi_range(0, int(homescreen.y))
+		star.position = Vector2(random_x,random_y)
+		
+		# animation speed
+		star.speed_scale =rng.randf_range(0.6,1.4)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-
+	
 func _on_playgame_pressed() -> void:
 	$click.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://scense/level.tscn")
 	
 
 
 func _on_instructions_pressed() -> void:
 	$click.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://scense/instructions.tscn")
 	
 	
 func _on_exit_pressed() -> void:
+	$click.play()
+	await get_tree().create_timer(0.25).timeout
 	get_tree().quit()
 
 
--- scense/instructions.gd
+++ scense/instructions.gd
@@ -3,7 +3,17 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#Stars 
+	var istruction_screen := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $"stars-instructions".get_children():
+		#position 
+		var random_x=rng.randi_range(0, int(istruction_screen.x))
+		var random_y=rng.randi_range(0, int(istruction_screen.y))
+		star.position = Vector2(random_x,random_y)
+		
+		# animation speed
+		star.speed_scale =rng.randf_range(0.6,1.4)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
